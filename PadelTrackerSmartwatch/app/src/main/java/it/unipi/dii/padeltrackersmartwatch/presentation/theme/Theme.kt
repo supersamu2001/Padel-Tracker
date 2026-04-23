483892da0ecc72655b724f214cbe3a194265e2e6
supersamu2001 <samuele2001marchi@gmail.com>
2026-04-23 12:32:51 +0200

    Kotlin files for smartwatch

---
+++ PadelTrackerSmartwatch/app/src/main/java/it/unipi/dii/padeltrackersmartwatch/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package it.unipi.dii.padeltrackersmartwatch.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun PadelTrackerSmartwatchTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
